ccc75aabadc1dd4062df1bbdf97aa0e269c73cae
Trista Lin <st920090st920090@gmail.com>
2020-08-24 16:47:27 +0800

    Modify code construction of loadingpage and chatinput

--- src/Component/Mainpage/ChatApp/ChatInput.js
+++ src/Component/Mainpage/ChatApp/ChatInput.js
@@ -3,70 +3,6 @@ import {db} from "../../../utils/firebase";
 // redux
 import {connect} from "react-redux";
 
-class EmojiBtn extends React.Component{
-  constructor(props){
-    super(props);
-  }
-  render(){
-    return <button onClick={this.props.handleEmojisToShow}>
-      <img src="/images/chat-input-add.png" />
-    </button>;
-  }
-}
-class EmojiList extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      emojiList: [
-        "&#128512;", "&#128513;", "&#128514;","&#129315;", "&#128515;", 
-        "&#128516;", "&#128517;", "&#128518;", "&#128521;", "&#128522;",	
-        "&#128523;", "&#128526;", "&#128525;", "&#128536;",	"&#128535;",	
-        "&#128537;", "&#128538;", "&#128578;", "&#129303;",	"&#129300;",	
-        "&#128528;", "&#128529;", "&#128566;", "&#128580;", "&#128527;",	
-        "&#128547;", "&#128549;", "&#128558;", "&#129296;", "&#128559;",
-        "&#128554;", "&#128555;", "&#128564;", "&#128524;", "&#129299;",	
-        "&#128539;", "&#128540;", "&#128541;", "&#129316;", "&#128530;",	
-        "&#128531;", "&#128532;", "&#128533;", "&#128579;",	"&#129297;",
-        "&#128562;", "&#128577;", "&#128534;", "&#128542;",	"&#128543;",	
-        "&#128548;", "&#128546;", "&#128557;", "&#128550;",	"&#128551;",	
-        "&#128552;", "&#128553;", "&#128556;", "&#128560;",	"&#128561;",	
-        "&#128563;", "&#128565;", "&#128545;", "&#128544;", "&#128519;",	
-        "&#129312;", "&#129313;", "&#129317;", "&#128567;",	"&#129298;",	
-        "&#129301;", "&#129314;", "&#129319;", "&#128520;",	"&#128127;",	
-        "&#128121;", "&#128122;", "&#128128;", "&#128123;",	"&#128125;",	
-        "&#128126;", "&#129302;", "&#128169;", "&#128570;",	"&#128568;",	
-        "&#128569;", "&#128571;", "&#128572;", "&#128573;",	"&#128576;",	
-        "&#128575;", "&#128574;", "&#128584;", "&#128585;", "&#128586;",
-        "&#128129;", "&#127995;", "&#127996;", "&#127997;", "&#127998;",	
-        "&#127999;", "&#128587;", "&#127995;", "&#127996;", "&#127997;",	
-        "&#127998;", "&#127999;",		
-      ]
-    };
-  }
-  pickEmoji(e){
-    this.props.handleInputPlusEmoji(e.target.textContent);
-    this.props.handleEmojisToShow();
-  }
-  render(){
-    let emojiList = "";
-    if(this.props.showEmojis){
-      emojiList = [];
-      for(let i = 0; i<this.state.emojiList.length; i++){
-        let item = <span 
-          dangerouslySetInnerHTML={{__html: this.state.emojiList[i]+" "}} 
-          key={i}
-          className="my-emoji"
-          onClick={this.pickEmoji.bind(this)}
-        ></span>;
-        emojiList.push(item);
-      }
-    }
-    return <div className="emojis" style={{display: this.props.showEmojis ? "block" : "none"}}>
-      {emojiList}
-    </div>;
-  }
-}
-
 class ChatInput extends React.Component{
   constructor(props){
     super(props);
@@ -146,8 +82,68 @@ class ChatInput extends React.Component{
   }
 }
 
+const EmojiBtn = (props)=>{
+  return <button onClick={props.handleEmojisToShow}>
+      <img src="/images/chat-input-add.png" />
+    </button>;
+};
+class EmojiList extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      emojiList: [
+        "&#128512;", "&#128513;", "&#128514;","&#129315;", "&#128515;", 
+        "&#128516;", "&#128517;", "&#128518;", "&#128521;", "&#128522;",	
+        "&#128523;", "&#128526;", "&#128525;", "&#128536;",	"&#128535;",	
+        "&#128537;", "&#128538;", "&#128578;", "&#129303;",	"&#129300;",	
+        "&#128528;", "&#128529;", "&#128566;", "&#128580;", "&#128527;",	
+        "&#128547;", "&#128549;", "&#128558;", "&#129296;", "&#128559;",
+        "&#128554;", "&#128555;", "&#128564;", "&#128524;", "&#129299;",	
+        "&#128539;", "&#128540;", "&#128541;", "&#129316;", "&#128530;",	
+        "&#128531;", "&#128532;", "&#128533;", "&#128579;",	"&#129297;",
+        "&#128562;", "&#128577;", "&#128534;", "&#128542;",	"&#128543;",	
+        "&#128548;", "&#128546;", "&#128557;", "&#128550;",	"&#128551;",	
+        "&#128552;", "&#128553;", "&#128556;", "&#128560;",	"&#128561;",	
+        "&#128563;", "&#128565;", "&#128545;", "&#128544;", "&#128519;",	
+        "&#129312;", "&#129313;", "&#129317;", "&#128567;",	"&#129298;",	
+        "&#129301;", "&#129314;", "&#129319;", "&#128520;",	"&#128127;",	
+        "&#128121;", "&#128122;", "&#128128;", "&#128123;",	"&#128125;",	
+        "&#128126;", "&#129302;", "&#128169;", "&#128570;",	"&#128568;",	
+        "&#128569;", "&#128571;", "&#128572;", "&#128573;",	"&#128576;",	
+        "&#128575;", "&#128574;", "&#128584;", "&#128585;", "&#128586;",
+        "&#128129;", "&#127995;", "&#127996;", "&#127997;", "&#127998;",	
+        "&#127999;", "&#128587;", "&#127995;", "&#127996;", "&#127997;",	
+        "&#127998;", "&#127999;",		
+      ]
+    };
+  }
+  pickEmoji(e){
+    this.props.handleInputPlusEmoji(e.target.textContent);
+    this.props.handleEmojisToShow();
+  }
+  render(){
+    let emojiList = "";
+    if(this.props.showEmojis){
+      emojiList = [];
+      for(let i = 0; i<this.state.emojiList.length; i++){
+        let item = <span 
+          dangerouslySetInnerHTML={{__html: this.state.emojiList[i]+" "}} 
+          key={i}
+          className="my-emoji"
+          onClick={this.pickEmoji.bind(this)}
+        ></span>;
+        emojiList.push(item);
+      }
+    }
+    return <div className="emojis" style={{display: this.props.showEmojis ? "block" : "none"}}>
+      {emojiList}
+    </div>;
+  }
+}
+
 const mapStateToProps = (store)=>{
   return{user: store.userReducer.user};
 };
 ChatInput = connect(mapStateToProps)(ChatInput);
+
 export {ChatInput, EmojiList};
